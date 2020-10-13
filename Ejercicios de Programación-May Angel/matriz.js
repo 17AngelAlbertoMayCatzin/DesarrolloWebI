7b3c559533787cabcbdace7c9e60eedfce4d157f
17AngelAlbertoMayCatzin <48849569+17AngelAlbertoMayCatzin@users.noreply.github.com>
2020-10-13 12:28:00 -0500

    Add files via upload

--- Ejercicios de Programación-May Angel/matriz.js
+++ Ejercicios de Programación-May Angel/matriz.js
@@ -6,16 +6,21 @@ var tbl = document.getElementById("tabla");
 var tblBody = document.createElement("tbody");
     for (var i = 0; i < columna; i++) {
        var fila = document.createElement("tr");
+       fila.setAttribute('style','background-color: aqua;')
        for (var j = 0; j < filas; j++) {
           var celda = document.createElement("td");
+          celda.setAttribute('style','background-color: red;')
           if(i==j){
               textoCelda = document.createTextNode(1);
               fila.appendChild(textoCelda);
+              
           } else{
               textoCelda1 = document.createTextNode(0);
               fila.appendChild(textoCelda1);
+              
           }
        }
+       
         tblBody.appendChild(fila);
     }
     tbl.appendChild(tblBody);
