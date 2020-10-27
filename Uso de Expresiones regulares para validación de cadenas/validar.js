4a7a0ee0f7b2f23a64fbe435be153dcb90fb5773
17AngelAlbertoMayCatzin <48849569+17AngelAlbertoMayCatzin@users.noreply.github.com>
2020-10-26 22:11:58 -0600

    Add files via upload
    Actualización: Uso de Expresiones regulares para validación de cadenas

--- Uso de Expresiones regulares para validación de cadenas/validar.js
+++ Uso de Expresiones regulares para validación de cadenas/validar.js
@@ -2,14 +2,22 @@ var isbnV;  //Variable que almacena el isbn ingresado por usuario
 var idJava; //Variable que almacena el identificador ingresado por usuario
 var expR1;  //Variable que almacena la expresión regular para validar el isbn
 var expR2;  //Variable que almacena la expresión regular para validar el identificador
-var valor1; //Variable
-var valor2; //Variable
+var valor1; //Variable para validar
+var valor2; //Variable para validar
+var palabrasReservadas; //Variable para almacenar las palabras reservadas de Java
+var contieneEle;        //Variable para validar
 //Función para procesar el formulario
 function validarDatos(){
+    //Asignamos las palabras reservadas de Java
+    palabrasReservadas = ["for", "abstract" ,"continue", "new","switch","assert", "default", "goto", "package","synchronized",
+    "boolean","do", "if", "private", "this", "break","double","implements", "protected", "throw", "byte","else","import","public",
+    "throws","case","enum","instanceof","return","transient", "catch","extends",  
+    "int","short","try","char","final","interface","static","void","class","finally","long","strictfp","volatile","const","float",
+    "native","super","while"]
     //Agregamos a la vairiable la expresión regular del isbn
-    expR1 = new RegExp('97[89]-\\d{1,5}-\\d{1,7}-\\d{1,6}-\\d{1}$'); 
+    expR1 = new RegExp('97[89][-\\s]+\\d{1,5}[-\\s]+\\d{1,7}[-\\s]+\\d{1,6}[-\\s]+\\d{1}$'); 
     //Agregamos a la vairiable la expresión regular del identificador
-    expR2 = new RegExp('^\\S[a-z]+$', 'i');
+    expR2 = new RegExp('^[_]*[A-Za-z]+[_]*[A-Za-z]+[0-9]*$', 'i');
     //Optenemos y guardamos el valor del input en la variable isbn
      isbnV= document.getElementById("isbn").value;
      //Optenemos y guardamos el valor del input en la variable idJava
@@ -30,8 +38,18 @@ function validarDatos(){
     //-------test(idJava) Comprueba si la expresión regular (expR2) casa con el texto (idJava) pasado por parámetro.
         if (expR2.test(idJava)==true){
             alert('Identificador correcto') //Mandamos una alerta cuando el usuario ingrese un identificador válido
-            valor2=true;                     //Asignamos true a la variable valor2, para su posterior validación
-            document.getElementById("errorIj").innerHTML=''     //Limpiamos el contenido de la etiqueta errorIb
+            ver= palabrasReservadas.includes( idJava ) //Buscamos un elemento en un array con includes, devolvera true si esta.
+            //Validamos si se encontro el elemento
+            if(ver==true){
+                alert('Ingreso una palabra reservada de Java')  //Mandamos una alerta cuando el usuario ingrese una palabra reservada de Java
+                document.getElementById("idenJava").value = ""; //Asignamos nulo al input idenJava
+                //A la etiqueta errorIj se le asigna un contenido, mostrando el error y un ejemplo.
+                document.getElementById("errorIj").innerHTML='El dato introducido es un identificador de java.'+'<br/> Introduzca un valor válido.'+ '<br/> Ejemplo: myVariable, Persona, validarNumero'
+            } else{
+                valor2=true;                                        //Asignamos true a la variable valor2, para su posterior validación
+                document.getElementById("errorIj").innerHTML=''     //Limpiamos el contenido de la etiqueta errorIb
+            }
+
         } else{
             alert('Identificador inválido') //Mandamos una alerta cuando el usuario ingrese un identificaor invalido
             document.getElementById("idenJava").value = ""; //Asignamos nulo al input idenJava
